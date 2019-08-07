13f45b6987e23bfeb9fd762d941b534d30582615
sarinnsreedharan <sarinns5379@gmail.com>
2019-08-07 16:21:23 +0530

    changes

---
+++ series.py
@@ -0,0 +1,24 @@
+n=int(raw_input())
+even=0
+odd=0
+if(n%2==0):
+    even=1
+    index=n/2
+else:
+    odd=1
+    index=n/2+1
+listodd=[]
+listeven=[]
+if(even):
+    for i in range(0,index):
+        evenvalue=i*6
+        listeven.append(evenvalue)
+    popvalue=listeven.pop()
+    print(popvalue)
+else:
+    for i in range(0,index):
+        oddvalue=i*7
+        listodd.append(oddvalue)
+    popvalue=listodd.pop()
+    print(popvalue)
+
